ca7751bca93935948c891b1bdb7220825d19e940
Sandeep Gangwar <Sandeep.Gangwar@silicus.com>
2019-05-21 13:42:08 +0530

    first commit

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading;
+using Hangfire;
+using System.Diagnostics;
+
+namespace TodoApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+
+        public void RunInBackground()
+        {
+            Debug.WriteLine($"Running in background Thread:{ Thread.CurrentThread.Name }");
+        }
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            BackgroundJob.Enqueue(()=> RunInBackground());
+            //Debug.WriteLine($"Running in  Thread:{ Thread.CurrentThread.Name }");
+            return new string[] { "value2", "value1" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
